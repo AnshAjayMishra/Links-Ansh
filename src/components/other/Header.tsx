e4e44353a0c36c2de1f758fced0a6b0db193bbbe
Ansh Ajay Mishra <45829312+AnshAjayMishra@users.noreply.github.com>
2025-03-23 16:43:34 +0530

    Update Header.tsx

--- src/components/other/Header.tsx
+++ src/components/other/Header.tsx
@@ -161,7 +161,7 @@ function Page() {
               </header>
 
               <div className="relative flex items-center justify-center mt-6 space-x-5">
-                <Link href="https://anshajaymishra.vercel.app">
+                <Link href="https://anshajaymishra.vercel.app" target="_blank">
                   <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full dark:bg-[#0b0a0a] bg-gray-100 px-8 py-1 text-sm font-medium text-black dark:text-gray-50 backdrop-blur-3xl">
@@ -169,7 +169,7 @@ function Page() {
                     </span>
                   </button>
                 </Link>
-                <Link href="mailto:ajayanshmishra10@gmail.com">
+                <Link href="mailto:ajayanshmishra10@gmail.com" target="_blank">
                   <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full dark:bg-[#0b0a0a] bg-gray-100 px-8 py-1 text-sm font-medium text-black dark:text-gray-50 backdrop-blur-3xl">
